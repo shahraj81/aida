b35ef38e93dd45e9cc67ee2dae618fe64a42e941
Shahzad Rajput <shahzad.rajput@gmail.com>
2018-06-18 13:36:03 -0400

    slots handled

--- utils/translate-annotations/seedling/AIDA-Translate-MASTER.pl
+++ utils/translate-annotations/seedling/AIDA-Translate-MASTER.pl
@@ -5,6 +5,8 @@ use TranslateManagerLib;
 
 my ($filename, $filehandler, $header, $entries, $i);
 
+my %mappings;
+
 # Load parent children DOCID mapping from parent_children.tab
 my $documents = Documents->new();
 my $documentelements = DocumentElements->new();
@@ -111,8 +113,8 @@ foreach my $entry( $entries->toarray() ){
 	$i++;
 	#print "ENTRY # $i:\n", $entry->tostring(), "\n";
 	my $document_eid = $entry->get("provenance");
-  my $thedocumentelement = $documentelements->get("BY_KEY", $document_eid);
-  my $thedocumentelementmodality = $thedocumentelement->get("TYPE");
+	my $thedocumentelement = $documentelements->get("BY_KEY", $document_eid);
+	my $thedocumentelementmodality = $thedocumentelement->get("TYPE");
 	my $document_id = $thedocumentelement->get("DOCUMENTID");
 	my $mention = Mention->new();
 	my $span = Span->new(
@@ -125,24 +127,32 @@ foreach my $entry( $entries->toarray() ){
 	$justification->add_span($span);
 	$mention->add_justification($justification);
 	$mention->set("MODALITY", $thedocumentelementmodality);
-  $mention->set("MENTIONID", $entry->get("entitymention_id"));
-  $mention->set("TEXT_STRING", $entry->get("text_string"));
-  $mention->set("JUSTIFICATION_STRING", $entry->get("justification"));
-  $mention->set("TREEID", $entry->get("tree_id"));
-  $mention->set("TYPE", $entry->get("type"));
+	$mention->set("MENTIONID", $entry->get("entitymention_id"));
+	$mention->set("ENTITYID", $entry->get("entity_id"));
+	$mention->set("TEXT_STRING", $entry->get("text_string"));
+	$mention->set("JUSTIFICATION_STRING", $entry->get("justification"));
+	$mention->set("TREEID", $entry->get("tree_id"));
+	$mention->set("TYPE", $entry->get("type"));
 	
 	my $entity = $entities->get("BY_KEY", $entry->get("kb_id"));
 	
-  $entity->set("KBID", $entry->get("kb_id")) unless $entity->set("KBID");
+	$entity->set("KBID", $entry->get("kb_id")) unless $entity->set("KBID");
 	$entity->set("TYPE", $entry->get("type")) unless $entity->set("TYPE");
 	$entity->add_mention($mention);
+	
+	$mappings{$mention->get("MENTIONID")} = {
+		KBID => $entity->get("KBID"),
+		TYPE => $mention->get("TYPE"),
+	};
+	$mappings{$mention->get("ENTITYID")} = {
+		KBID => $entity->get("KBID"),
+		TYPE => $mention->get("TYPE"),
+	};
 }
 
 #####################################################################################
 # Print entity mentions from T101_ent_mentions.tab in RDF Turtle format
 #####################################################################################
-
-
 
 foreach my $entity($entities->toarray()) {
   #&Super::dump_structure($entity, 'Entity');
@@ -151,7 +161,7 @@ foreach my $entity($entities->toarray()) {
   my $node_id = $entity->get("KBID");
   my $entity_type = $entity->get("TYPE");
   
-  print OUTPUT "ldc:$node_id a aif:Entity ;\n";
+  print OUTPUT "\n\nldc:$node_id a aif:Entity ;\n";
   print OUTPUT "  aif:system ldc:LDCModelGenerator ;\n";
   
   foreach my $entity_mention($entity->get("MENTIONS")->toarray()) {
@@ -159,6 +169,7 @@ foreach my $entity($entities->toarray()) {
   	my $entity_mention_type = $entity_mention->get("TYPE");
     my ($justification_source) = $entity_mention->get("SOURCE_DOCUMENT_ELEMENTS");
   	my $justification_type = $entity_mention->get("MODALITY");
+    my $text_string = $entity_mention->get("TEXT_STRING");
   	if($justification_type eq "nil") {
       print "--skipping over $entity_mention_id due to unknown modality\n";
       next;
@@ -192,6 +203,7 @@ foreach my $entity($entities->toarray()) {
     print OUTPUT "                                                aif:system              ldc:LDCModelGenerator ;\n";
     print OUTPUT "                                              ] ;\n";
     print OUTPUT "                     aif:ID                   \"$entity_mention_id\" ;\n";
+    print OUTPUT "                     aif:textString           \"$text_string\" ;\n";
     print OUTPUT "                     aif:source               \"$justification_source\" ;\n";
     
     if($justification_type eq "aif:TextJustification") {
@@ -244,23 +256,32 @@ foreach my $entry( $entries->toarray() ){
   $mention->add_justification($justification);
   $mention->set("MODALITY", $thedocumentelementmodality);
   $mention->set("MENTIONID", $entry->get("eventmention_id"));
+  $mention->set("EVENTID", $entry->get("event_id"));
   $mention->set("TEXT_STRING", $entry->get("text_string"));
   $mention->set("JUSTIFICATION_STRING", $entry->get("justification"));
   $mention->set("TREEID", $entry->get("tree_id"));
   $mention->set("TYPE", $entry->get("type"));
+  $mention->set("SUBTYPE", $entry->get("subtype"));
   
   my $entity = $entities->get("BY_KEY", $entry->get("kb_id"));
   
   $entity->set("KBID", $entry->get("kb_id")) unless $entity->set("KBID");
   $entity->set("TYPE", $entry->get("type")) unless $entity->set("TYPE");
   $entity->add_mention($mention);
+  
+  $mappings{$mention->get("MENTIONID")} = {
+		KBID => $entity->get("KBID"),
+		TYPE => $mention->get("TYPE").".".$mention->get("SUBTYPE"),
+	};
+  $mappings{$mention->get("EVENTID")} = {
+		KBID => $entity->get("KBID"),
+		TYPE => $mention->get("TYPE").".".$mention->get("SUBTYPE"),
+	};
 }
 
 #####################################################################################
 # Print entity mentions from T101_evt_mentions.tab in RDF Turtle format
 #####################################################################################
-
-
 
 foreach my $entity($entities->toarray()) {
   #&Super::dump_structure($entity, 'Entity');
@@ -274,9 +295,10 @@ foreach my $entity($entities->toarray()) {
   
   foreach my $entity_mention($entity->get("MENTIONS")->toarray()) {
     my $entity_mention_id = $entity_mention->get("MENTIONID");
-    my $entity_mention_type = $entity_mention->get("TYPE");
+    my $entity_mention_type = $entity_mention->get("TYPE").".".$entity_mention->get("SUBTYPE");
     my ($justification_source) = $entity_mention->get("SOURCE_DOCUMENT_ELEMENTS");
     my $justification_type = $entity_mention->get("MODALITY");
+    my $text_string = $entity_mention->get("TEXT_STRING");
     
     if($justification_type eq "nil") {
     	print "--skipping over $entity_mention_id due to unknown modality\n";
@@ -312,6 +334,7 @@ foreach my $entity($entities->toarray()) {
     print OUTPUT "                                                aif:system              ldc:LDCModelGenerator ;\n";
     print OUTPUT "                                              ] ;\n";
     print OUTPUT "                     aif:ID                   \"$entity_mention_id\" ;\n";
+    print OUTPUT "                     aif:textString           \"$text_string\" ;\n";
     print OUTPUT "                     aif:source               \"$justification_source\" ;\n";
     
     if($justification_type eq "aif:TextJustification") {
@@ -362,20 +385,31 @@ foreach my $entry( $entries->toarray() ){
   $mention->add_justification($justification);
   $mention->set("MODALITY", $thedocumentelementmodality);
   $mention->set("MENTIONID", $entry->get("relationmention_id"));
+  $mention->set("RELATIONID", $entry->get("relation_id"));
   $mention->set("TEXT_STRING", $entry->get("text_string"));
   $mention->set("JUSTIFICATION_STRING", $entry->get("justification"));
   $mention->set("TREEID", $entry->get("tree_id"));
   $mention->set("TYPE", $entry->get("type"));
+  $mention->set("SUBTYPE", $entry->get("subtype"));
   
   my $entity = $entities->get("BY_KEY", $entry->get("kb_id"));
   
   $entity->set("KBID", $entry->get("kb_id")) unless $entity->set("KBID");
   $entity->set("TYPE", $entry->get("type")) unless $entity->set("TYPE");
   $entity->add_mention($mention);
+  
+  $mappings{$mention->get("MENTIONID")} = {
+		KBID => $entity->get("KBID"),
+		TYPE => $mention->get("TYPE").".".$mention->get("SUBTYPE"),
+	};
+  $mappings{$mention->get("RELATIONID")} = {
+		KBID => $entity->get("KBID"),
+		TYPE => $mention->get("TYPE").".".$mention->get("SUBTYPE"),
+	};
 }
 
 #####################################################################################
-# Print entity mentions from T101_evt_mentions.tab in RDF Turtle format
+# Print entity mentions from T101_rel_mentions.tab in RDF Turtle format
 #####################################################################################
 
 
@@ -392,9 +426,10 @@ foreach my $entity($entities->toarray()) {
   
   foreach my $entity_mention($entity->get("MENTIONS")->toarray()) {
     my $entity_mention_id = $entity_mention->get("MENTIONID");
-    my $entity_mention_type = $entity_mention->get("TYPE");
+    my $entity_mention_type = $entity_mention->get("TYPE").".".$entity_mention->get("SUBTYPE");
     my ($justification_source) = $entity_mention->get("SOURCE_DOCUMENT_ELEMENTS");
     my $justification_type = $entity_mention->get("MODALITY");
+    my $text_string = $entity_mention->get("TEXT_STRING");
     
     if($justification_type eq "nil") {
       print "--skipping over $entity_mention_id due to unknown modality\n";
@@ -430,6 +465,7 @@ foreach my $entity($entities->toarray()) {
     print OUTPUT "                                                aif:system              ldc:LDCModelGenerator ;\n";
     print OUTPUT "                                              ] ;\n";
     print OUTPUT "                     aif:ID                   \"$entity_mention_id\" ;\n";
+    print OUTPUT "                     aif:textString           \"$text_string\" ;\n";
     print OUTPUT "                     aif:source               \"$justification_source\" ;\n";
     
     if($justification_type eq "aif:TextJustification") {
@@ -449,7 +485,75 @@ foreach my $entity($entities->toarray()) {
   }
 }
 
+#####################################################################################
+# Process T101_rel_slots.tab and print in turle-RDF format
+#####################################################################################
 
+$filename = "data/LDC2018E45_AIDA_Scenario_1_Seedling_Annotation_V2.0/data/T101/T101_rel_slots.tab";
+$filehandler = FileHandler->new($filename);
+$header = $filehandler->get("HEADER");
+$entries = $filehandler->get("ENTRIES"); 
+
+# variable to hold entities
+$entities = Entities->new();
+$i=0;
+foreach my $entry( $entries->toarray() ){
+  $i++;
+  #print "ENTRY # $i:\n", $entry->tostring(), "\n";
+  my $mention_id = $entry->get("relationmention_id");
+  my $slot_type = $entry->get("slot_type");
+  my $arg_id = $entry->get("arg_id");
+  my $mention_kb_id = $mappings{$mention_id}{KBID};
+  my $relation_type = $mappings{$mention_id}{TYPE};
+  my $arg_kb_id = $mappings{$arg_id}{KBID};
+  
+  print OUTPUT "\n\n[ a                rdf:Statement ;\n";
+  print OUTPUT "  rdf:object       $arg_kb_id ;\n";
+  print OUTPUT "  rdf:predicate    $relation_type\_$slot_type ;\n";
+  print OUTPUT "  rdf:subject      $mention_kb_id ;\n";
+  print OUTPUT "  aif:confidence   [ a                        aif:Confidence ;\n";
+  print OUTPUT "                     aif:confidenceValue      \"1.0\"^^<http://www.w3.org/2001/XMLSchema#double> ;\n";
+  print OUTPUT "                     aif:system               ldc:LDCModelGenerator ;\n";
+  print OUTPUT "                   ] ;\n";
+  print OUTPUT "  aif:system       ldc:LDCModelGenerator ;\n";
+  print OUTPUT "] .\n"; 
+
+}
+
+#####################################################################################
+# Process T101_evt_slots.tab and print in turle-RDF format
+#####################################################################################
+
+$filename = "data/LDC2018E45_AIDA_Scenario_1_Seedling_Annotation_V2.0/data/T101/T101_evt_slots.tab";
+$filehandler = FileHandler->new($filename);
+$header = $filehandler->get("HEADER");
+$entries = $filehandler->get("ENTRIES"); 
+
+# variable to hold entities
+$entities = Entities->new();
+$i=0;
+foreach my $entry( $entries->toarray() ){
+  $i++;
+  #print "ENTRY # $i:\n", $entry->tostring(), "\n";
+  my $mention_id = $entry->get("eventmention_id");
+  my $slot_type = $entry->get("slot_type");
+  my $arg_id = $entry->get("arg_id");
+  my $mention_kb_id = $mappings{$mention_id}{KBID};
+  my $event_type = $mappings{$mention_id}{TYPE};
+  my $arg_kb_id = $mappings{$arg_id}{KBID};
+  
+  print OUTPUT "\n\n[ a                rdf:Statement ;\n";
+  print OUTPUT "  rdf:object       $arg_kb_id ;\n";
+  print OUTPUT "  rdf:predicate    $event_type\_$slot_type ;\n";
+  print OUTPUT "  rdf:subject      $mention_kb_id ;\n";
+  print OUTPUT "  aif:confidence   [ a                        aif:Confidence ;\n";
+  print OUTPUT "                     aif:confidenceValue      \"1.0\"^^<http://www.w3.org/2001/XMLSchema#double> ;\n";
+  print OUTPUT "                     aif:system               ldc:LDCModelGenerator ;\n";
+  print OUTPUT "                   ] ;\n";
+  print OUTPUT "  aif:system       ldc:LDCModelGenerator ;\n";
+  print OUTPUT "] .\n"; 
+
+}
 close(OUTPUT);
 
 
