75efee08e9dcb4aa5d648eb367aa076fb5bb672c
Shahzad Rajput <shahzad.rajput@gmail.com>
2018-09-10 12:49:33 -0400

    single entrypoint simplified graph query id fixed

--- utils/generate-queries/GenerateQueriesManagerLib.pm
+++ utils/generate-queries/GenerateQueriesManagerLib.pm
@@ -1645,18 +1645,19 @@ sub generate_graph_queries {
 		my @matching_cannoical_mentions = $self->get("CANONICAL_MENTIONS")->get("WHERE", "NODEID", $node->get("NODEID"));
 		next unless @matching_cannoical_mentions;
 		$i++;
-		my $query_id = "$query_id_prefix\_$i\_0";
+		my $composite_query_id = "$query_id_prefix\_$i\_0";
 		my $composite_query = GraphQuery->new($self->get("LOGGER"),
 											$self->get("KEYFRAMES_BOUNDINGBOXES"),
 											$self->get("IMAGES_BOUNDINGBOXES"),
-											$self->get("DOCUMENTIDS_MAPPINGS"), $query_id, $edges->toarray());
+											$self->get("DOCUMENTIDS_MAPPINGS"), $composite_query_id, $edges->toarray());
 		my $j = 0;
 		foreach my $canonical_mention(@matching_cannoical_mentions) {
 			$j++;
+			my $single_entrypoint_query_id = "$query_id_prefix\_$i\_$j";
 			my $single_entrypoint_query = GraphQuery->new($self->get("LOGGER"),
 											$self->get("KEYFRAMES_BOUNDINGBOXES"),
 											$self->get("IMAGES_BOUNDINGBOXES"),
-											$self->get("DOCUMENTIDS_MAPPINGS"), $query_id, $edges->toarray());
+											$self->get("DOCUMENTIDS_MAPPINGS"), $single_entrypoint_query_id, $edges->toarray());
 			$composite_query->add_entrypoint($canonical_mention);
 			$single_entrypoint_query->add_entrypoint($canonical_mention);
 			$queries->add($single_entrypoint_query);
