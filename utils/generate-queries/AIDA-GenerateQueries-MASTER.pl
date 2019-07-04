a805753eab547938b7d62890486378f9a9600b7c
Shahzad Rajput <shahzad.rajput@gmail.com>
2019-07-04 11:31:13 -0400

    code to genenrate queries updated to take generate evaluation queries in addition to practice queries

--- utils/generate-queries/AIDA-GenerateQueries-MASTER.pl
+++ utils/generate-queries/AIDA-GenerateQueries-MASTER.pl
@@ -8,6 +8,20 @@ my $error_filename = "problems.log";
 $logger->set_error_output($error_filename);
 my $error_output = $logger->get_error_output();
 
+my $data_type = "evaluation";
+my %data_type_topic_and_pt_ids = (
+      practice => [qw(R103_PT001 R103_PT002 R103_PT003 R103_PT004 R103_PT005 R105_PT001 R105_PT002 R107_PT001 R107_PT002)],
+      evaluation => [qw(E101_PT001 E101_PT002 E101_PT003 E101_PT004 E101_PT005 E101_PT006 E101_PT007
+                        E102_PT001 E102_PT002 E102_PT003 E102_PT004 E102_PT005
+                        E103_PT001 E103_PT002 E103_PT003)],
+   );
+my %ref_kbid_prefixes = (
+      practice => "LDC2019E44",
+      evaluation => "LDC2019E43",
+   );
+my @topic_and_pt_ids = @{$data_type_topic_and_pt_ids{$data_type}};
+my $ref_kbid_prefix = $ref_kbid_prefixes{$data_type};
+
 my $dtd_files = Container->new("String");
 $dtd_files->add("2019/input/queries_dtd/class_query.dtd", "class_query");
 $dtd_files->add("2019/input/queries_dtd/zerohop_query.dtd", "zerohop_query");
@@ -19,15 +33,8 @@ $ontology_files->add("2019/input/ontology/LDC_AIDAAnnotationOntologyWithMapping_
 $ontology_files->add("2019/input/ontology/LDC_AIDAAnnotationOntologyWithMapping_V8_relations.tab", "relations");
 
 my $prevailingtheory_files = Container->new("String");
-$prevailingtheory_files->add("2019/input/prevailing-theories-practice/R103_PT001.tab", "R103_PT001");
-$prevailingtheory_files->add("2019/input/prevailing-theories-practice/R103_PT002.tab", "R103_PT002");
-$prevailingtheory_files->add("2019/input/prevailing-theories-practice/R103_PT003.tab", "R103_PT003");
-$prevailingtheory_files->add("2019/input/prevailing-theories-practice/R103_PT004.tab", "R103_PT004");
-$prevailingtheory_files->add("2019/input/prevailing-theories-practice/R103_PT005.tab", "R103_PT005");
-$prevailingtheory_files->add("2019/input/prevailing-theories-practice/R105_PT001.tab", "R105_PT001");
-$prevailingtheory_files->add("2019/input/prevailing-theories-practice/R105_PT002.tab", "R105_PT002");
-$prevailingtheory_files->add("2019/input/prevailing-theories-practice/R107_PT001.tab", "R107_PT001");
-$prevailingtheory_files->add("2019/input/prevailing-theories-practice/R107_PT002.tab", "R107_PT002");
+map {$prevailingtheory_files->add("2019/input/prevailing-theories-$data_type/$_.tab", $_)}
+  @topic_and_pt_ids;
 
 my $named_refkbids_file = "2019/input/named_reference_kb_nodes.txt";
 
@@ -42,7 +49,7 @@ $parameters->set("TA1_CLASS_QUERYID_PREFIX", "AIDA_TA1_CL_2019");
 $parameters->set("TA1_GRAPH_QUERYID_PREFIX", "AIDA_TA1_GR_2019");
 $parameters->set("TA2_ZEROHOP_QUERYID_PREFIX", "AIDA_TA2_ZH_2019");
 $parameters->set("TA2_GRAPH_QUERYID_PREFIX", "AIDA_TA2_GR_2019");
-$parameters->set("REFERENCE_KBID_PREFIX", "LDC2019E44");
+$parameters->set("REFERENCE_KBID_PREFIX", $ref_kbid_prefix);
 $parameters->set("TA2_GRAPH_QUERY_MAPPINGS_FILENAME", "TA2_GraphQuery_mappings.txt");
 
 $logger->NIST_die("Output directory exists") if -d $parameters->get("OUTPUT_DIR");
@@ -50,7 +57,6 @@ $logger->NIST_die("Output directory exists") if -d $parameters->get("OUTPUT_DIR"
 my $query_generator = QueryGenerator->new($logger, $parameters);
 
 my $year = "2019";
-my @topic_and_pt_ids = qw(R103_PT001 R103_PT002 R103_PT003 R103_PT004 R103_PT005 R105_PT001 R105_PT002 R107_PT001 R107_PT002);
 foreach my $topic_and_pt_id(@topic_and_pt_ids) {
 	my ($topic_id, $pt_id) = split("_", $topic_and_pt_id);
 	$query_generator->generate_queries($year, $topic_id, $pt_id);
