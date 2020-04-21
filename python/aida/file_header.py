111c702287d874b723929d466e95d447768e3acb
Shahzad Rajput <shahzad.rajput@gmail.com>
2020-04-21 13:24:37 -0400

    documentation added

--- python/aida/file_header.py
+++ python/aida/file_header.py
@@ -13,14 +13,26 @@ import re
 
 class FileHeader(Object):
     """
-    File header for AIDA.
+    The object represending the header of a tab separated file.
     """
 
     def __init__(self, logger, header_line):
+        """
+        Initializes the FileHeader using header_line.
+
+        Arguments:
+            logger (aida.Logger):
+                the aida.Logger object.
+            header_line (str):
+                the line used to generate the FileHeader object.
+        """
         super().__init__(logger)
         self.logger = logger
         self.line = header_line
         self.columns = list(re.split(r'\t', header_line))
     
     def __str__(self, *args, **kwargs):
+        """
+        Returns the string representation of the header.
+        """
         return self.line
