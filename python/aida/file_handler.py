6e2edd494fa165da192d126f4efc0b579118e5c8
Shahzad Rajput <shahzad.rajput@gmail.com>
2020-04-21 12:55:42 -0400

    documentation added

--- python/aida/file_handler.py
+++ python/aida/file_handler.py
@@ -16,18 +16,37 @@ import re
 class FileHandler(Object):
     """
     File handler for AIDA.
+
+    This FileHandler supports reading tab-separated files only.
     """
 
     def __init__(self, logger, filename, header=None, encoding=None):
+        """
+        Initializes this instance.
+        Arguments:
+            logger (aida.Logger):
+                the aida.Logger object
+            filename (str):
+                the name of the file including the path
+            header (aida.Header or None):
+                if provided, this header will be used for the file,
+                otherwise the header will be read from the first line.
+            encoding (str or None):
+                the encoding to be used for opening the file.
+        """
         super().__init__(logger)
         self.encoding = encoding
         self.filename = filename
         self.header = header
         self.logger = logger
+        # lines from the file are read into entries (aida.Entry)
         self.entries = []
         self.load_file()
     
     def load_file(self):
+        """
+        Load the file.
+        """
         with open(self.filename, encoding=self.encoding) as file:
             for lineno, line in enumerate(file, start=1):
                 if self.header is None:
@@ -42,4 +61,7 @@ class FileHandler(Object):
                     self.entries.append(entry)
     
     def __iter__(self):
+        """
+        Returns iterator over entries.
+        """
         return iter(self.entries)
