8b6633f4757035911a028b219eee26a518269edf
Shahzad Rajput <shahzad.rajput@gmail.com>
2020-04-21 09:06:23 -0400

    documentation added

--- python/aida/container.py
+++ python/aida/container.py
@@ -13,17 +13,37 @@ class Container(Object):
     """
     The AIDA container class.
 
-    Internally, the objects of this class store objects in a dictionary. 
+    Internally, the instance of this class stores objects in a dictionary.
     """
 
     def __init__(self, logger):
+        """
+        Initializes this instance, and sets the logger for newly created instance.
+
+        This is where the empty store is initialized.
+
+        Arguments:
+            logger (aida.Logger):
+                the aida.Logger object
+        """
         super().__init__(logger)
         self.store = {}
-    
+
     def __iter__(self):
+        """
+        Returns the iterator over the store.
+        """
         return iter(self.store)
-        
+
     def get(self, key, *args, default=None):
+        """
+        Gets the value for the key given args, if found. Returns None otherwise.
+
+        The value is looked up first in the parent object, returned if found. Otherwise,
+        the value is looked up in the store, again returned if found. Otherwise, the
+        key is added, to the store, with its value set to the default value provided
+        or None, if no default value was provided.
+        """
         value = super().get(key, *args)
         if value:
             return value
@@ -34,17 +54,28 @@ class Container(Object):
                 value = default
                 self.add(key=key, value=value)
             return value
-        
+
     def set(self, key, value):
+        """
+        Set the value of the key in the store if key is found in the store, otherwise,
+        the object's setter is called.
+        """
         if key in self.store:
             self.store[key] = value
         else:
             super().set(key, value)
-            
+
     def exists(self, key):
+        """
+        Returns True if key is found in the store, False otherwise.
+        """
         return key in self.store
 
     def add(self, value, key=None):
+        """
+        Add the value to the store and map it to the key if provided, otherwise,
+        use the length of the store as the key.
+        """
         if key is None:
             self.store[len(self.store)] = value
         else:
@@ -54,7 +85,13 @@ class Container(Object):
         return self.store
 
     def keys(self):
+        """
+        Returns a new view of the store's keys.
+        """
         return self.store.keys()
-    
+
     def values(self):
+        """
+        Returns a new view of the store’s values.
+        """
         return self.store.values()
