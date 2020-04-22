85f161542eec47187dd48729275dd2bae370ecc9
Shahzad Rajput <shahzad.rajput@gmail.com>
2020-04-22 06:42:39 -0400

    documentation added

--- python/aida/document.py
+++ python/aida/document.py
@@ -12,15 +12,33 @@ from aida.document_elements import DocumentElements
 
 class Document(Container):
     """
-    AIDA document class
+    The document class
     """
 
     def __init__(self, logger, id=None):
+        """
+        Initializes the document instance, setting the logger, and optionally its ID.
+
+        Arguments:
+            logger (aida.Logger):
+                the aida.Logger object
+            id (str):
+                the string identifier of the document
+
+        NOTE: the document contains a container to store document elements that
+        comprise this document.
+        """
         super().__init__(logger)
         self.document_elements = DocumentElements(logger)
         self.id = id
         self.logger = logger
-    
+
     def add_document_element(self, document_element):
+        """
+        Adds the document element to this document.
+
+        Arguments:
+            document_element (aida.DocumentELement)
+        """
         doceid = document_element.get('id')
         self.document_elements.add(key = doceid, value = document_element)
