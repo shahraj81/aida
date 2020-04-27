c17b26184a1716acfda8c7831cba42cdefe494b4
Shahzad Rajput <shahzad.rajput@gmail.com>
2020-04-27 11:38:39 -0400

    documentation added

--- python/aida/slot.py
+++ python/aida/slot.py
@@ -1,5 +1,5 @@
 """
-AIDA slot class.
+Class representing relation and event slots.
 """
 
 __author__  = "Shahzad Rajput <shahzad.rajput@nist.gov>"
@@ -11,10 +11,23 @@ from aida.object import Object
 
 class Slot(Object):
     """
-    AIDA slot class.
+    Class representing relation and event slots.
     """
     
     def __init__(self, logger, subject, slot_code, slot_type, argument, attribute, where):
+        """
+        Initialize the Slot.
+
+        Arguments:
+            logger (aida.Logger):
+                the aida.Logger object
+            subject (aida.Mention)
+            slot_code (str)
+            slot_type (str)
+            argument (aida.Mention)
+            attribute (str)
+            where (dict)
+        """
         super().__init__(logger)
         self.subject = subject
         self.slot_code = slot_code
@@ -24,8 +37,16 @@ class Slot(Object):
         self.where = where
 
     def is_negated(self):
+        """
+        Returns True if the Slot is negated, False otherwise.
+        """
         attributes = self.get('attribute')
         return attributes is not None and 'not' in attributes.split(',')
 
     def get_ID(self):
+        """
+        Gets the ID of the instance.
+
+        The ID is the concatenation of the subject ID, slot type, and the argument ID, separated by colon ':'
+        """
         return '{}:{}:{}'.format(self.get('subject').get('id'), self.get('slot_type'), self.get('argument').get('id'))
