8d1728334870bd41d1659a6702eee8b17fd36493
Shahzad Rajput <shahzad.rajput@gmail.com>
2020-07-14 13:00:19 -0400

    changing between hasName and textValue depending on the type of the entity

--- python/aida/aif_generator.py
+++ python/aida/aif_generator.py
@@ -144,15 +144,46 @@ def generate_ere_object_triples(reference_kb_id, ere_object):
     task1 document specific kbs.
     """
 
+    logger = ere_object.get('logger')
     ere_type = ere_object.get('node_metatype').capitalize()
+
+    default_predicate = 'hasName'
+    type_to_text_predicate_fieldname_mapping = {
+        'PER': 'hasName',
+        'ORG': 'hasName',
+        'GPE': 'hasName',
+        'FAC': 'hasName',
+        'LOC': 'hasName',
+        'WEA': 'hasName',
+        'VEH': 'hasName',
+        'LAW': 'hasName',
+        'TTL': 'hasName',
+        'MON': 'textValue',
+        'RES': 'textValue',
+        'VAL': 'textValue'
+        }
 
     # generate link assertion triples
     has_name_triples = []
     if ere_type == 'Entity':
+        text_predicate_fieldnames = {}
+        for top_level_type in ere_object.get('top_level_types'):
+            text_predicate_fieldname = default_predicate
+            if top_level_type not in type_to_text_predicate_fieldname_mapping:
+                logger.record_event('MISSING_ENTRY_IN_LOOKUP_WARNING', top_level_type, 'type_to_text_predicate_fieldname_mapping, using {} by default'.format(default_predicate))
+            else:
+                text_predicate_fieldname = type_to_text_predicate_fieldname_mapping[top_level_type]
+            text_predicate_fieldnames[text_predicate_fieldname] = 1
+        text_predicate_fieldname = default_predicate
+        if len(text_predicate_fieldnames) > 1:
+            logger.record_event('DEFAULT_CRITICAL_ERROR', '{} has more than one values for text_predicate_fieldname: {}'.format(ere_object.get('ID'), ','.join(text_predicate_fieldnames.keys())), ere_object.get('where'))
+        if len(text_predicate_fieldnames) == 1:
+            text_predicate_fieldname = list(text_predicate_fieldnames.keys())[0]
         text_strings = ere_object.get('text_strings')
         for text_string in text_strings:
-            if len(text_string) < 256 and 'nam' in text_strings[text_string]:
-                has_name_triple = 'ldc:{ere_object_id} aida:hasName "{text_string}" .'.format(ere_object_id=ere_object.get('ID'),
+            if (len(text_string) < 256 and 'nam' in text_strings[text_string]) or text_predicate_fieldname == 'textValue':
+                has_name_triple = 'ldc:{ere_object_id} aida:{text_predicate_fieldname} "{text_string}" .'.format(ere_object_id=ere_object.get('ID'),
+                                                                                          text_predicate_fieldname=text_predicate_fieldname,
                                                                                           text_string=text_string.replace('"', '\\"'))
                 has_name_triples.append(has_name_triple)
     node_ids = []
