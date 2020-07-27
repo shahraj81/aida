deff467b8e5822dfb8ea2bb42ba1ce53562c34a8
Shahzad Rajput <shahzad.rajput@gmail.com>
2020-07-27 12:24:13 -0400

    code location added for error handling

--- python/aida/cluster.py
+++ python/aida/cluster.py
@@ -27,7 +27,8 @@ class Cluster(Object):
         self.add_mention(entry.get('?mention_span'),
                          trim_cv(entry.get('?t_cv')),
                          trim_cv(entry.get('?cm_cv')),
-                         trim_cv(entry.get('?j_cv')))
+                         trim_cv(entry.get('?j_cv')),
+                         entry.get('where'))
 
     def add_metatype(self, metatype, where):
         if self.get('metatype') is None:
@@ -38,7 +39,7 @@ class Cluster(Object):
     def add_type(self, cluster_type):
         self.get('types').add(cluster_type, cluster_type)
 
-    def add_mention(self, span_string, t_cv, cm_cv, j_cv):
+    def add_mention(self, span_string, t_cv, cm_cv, j_cv, where):
         logger = self.get('logger')
         mention = Object(logger)
         mention.set('ID', span_string)
@@ -46,7 +47,7 @@ class Cluster(Object):
         mention.set('t_cv', t_cv)
         mention.set('cm_cv', cm_cv)
         mention.set('j_cv', j_cv)
-        mention.set('span', spanstring_to_object(logger, span_string))
+        mention.set('span', spanstring_to_object(logger, span_string, where))
         self.get('mentions').add(key=mention.get('ID'), value=mention)
 
     def __str__(self, *args, **kwargs):
