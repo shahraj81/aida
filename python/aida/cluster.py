d35666063675430a23d63a94dd7b6692cd272578
Shahzad Rajput <shahzad.rajput@gmail.com>
2020-07-27 12:23:06 -0400

    renamed: parse_cv->trim_cv; using spanstring_to_object

--- python/aida/cluster.py
+++ python/aida/cluster.py
@@ -3,7 +3,6 @@ AIDA Cluster class.
 
 This class supports the alignment of clusters as needed for scoring.
 """
-
 __author__  = "Shahzad Rajput <shahzad.rajput@nist.gov>"
 __status__  = "production"
 __version__ = "0.0.0.1"
@@ -11,9 +10,7 @@ __date__    = "15 July 2020"
 
 from aida.object import Object
 from aida.container import Container
-from aida.utility import parse_cv
-from aida.span import Span
-import re
+from aida.utility import trim_cv, spanstring_to_object
 
 class Cluster(Object):
 
@@ -28,9 +25,9 @@ class Cluster(Object):
         self.add_metatype(entry.get('?metatype'), entry.get('where'))
         self.add_type(entry.get('?type'))
         self.add_mention(entry.get('?mention_span'),
-                         parse_cv(entry.get('?t_cv')),
-                         parse_cv(entry.get('?cm_cv')),
-                         parse_cv(entry.get('?j_cv')))
+                         trim_cv(entry.get('?t_cv')),
+                         trim_cv(entry.get('?cm_cv')),
+                         trim_cv(entry.get('?j_cv')))
 
     def add_metatype(self, metatype, where):
         if self.get('metatype') is None:
@@ -49,17 +46,8 @@ class Cluster(Object):
         mention.set('t_cv', t_cv)
         mention.set('cm_cv', cm_cv)
         mention.set('j_cv', j_cv)
+        mention.set('span', spanstring_to_object(logger, span_string))
         self.get('mentions').add(key=mention.get('ID'), value=mention)
-        
-        pattern = re.compile('^(.*?):(.*?):\((\d+),(\d+)\)-\((\d+),(\d+)\)$')
-        match = pattern.match(span_string)
-        if match:
-            document_id = match.group(1)
-            document_eleemnt_id = match.group(2)
-            span = Span(logger, match.group(3), match.group(4), match.group(5), match.group(6))
-            mention.set('document_id', document_id)
-            mention.set('document_element_id', document_eleemnt_id)
-            mention.set('span', span)
 
     def __str__(self, *args, **kwargs):
         return self.get('ID')
