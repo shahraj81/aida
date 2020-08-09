e85501470f9f18d9a4e7baf34a24daba5bff3645
Shahzad Rajput <shahzad.rajput@gmail.com>
2020-08-09 13:15:01 -0400

    first draft of task1 response validator added

--- python/aida/response_set.py
+++ python/aida/response_set.py
@@ -19,175 +19,304 @@ from aida.utility import get_query_id_from_filename
 import os
   
 attributes = {
+    'argument_assertion_confidence': {
+        'name': 'argument_assertion_confidence',
+        'schemas': ['AIDA_PHASE2_TASK1_AM_RESPONSE'],
+        'tasks': ['task1'],
+        'validate': 'validate_confidence',
+        'years': [2020],
+        },
+    'cluster_id': {
+        'name': 'cluster_id',
+        'schemas': ['AIDA_PHASE2_TASK1_CM_RESPONSE', 'AIDA_PHASE2_TASK1_TM_RESPONSE'],
+        'tasks': ['task1'],
+        'years': [2020],
+        },
     'cluster_membership_confidence': {
         'name': 'cluster_membership_confidence',
-        'years': [2019],
+        'schemas': ['AIDA_PHASE2_TASK1_CM_RESPONSE'],
         'tasks': ['task1'],
-        'query_types': ['ClassQuery'],
-        'validate': 'validate_confidence'      
+        'validate': 'validate_confidence',
+        'years': [2020],
+        },
+    'cluster_type': {
+        'name': 'cluster_type',
+        'schemas': ['AIDA_PHASE2_TASK1_CM_RESPONSE'],
+        'tasks': ['task1'],
+        'years': [2020],
         },
     'document_id': {
         'name': 'document_id',
-        'years': [2019],
         'tasks': ['task1'],
-        'query_types': ['ClassQuery'],
+        'schemas': ['AIDA_PHASE2_TASK1_AM_RESPONSE', 'AIDA_PHASE2_TASK1_CM_RESPONSE'],
         'validate': 'validate_document_id',
-        'dependencies': ['kb_document_id']
+        'generate': 'generate_document_id',
+        'years': [2020],
+        },
+    'end_before_day': {
+        'name': 'end_before_day',
+        'schemas': ['AIDA_PHASE2_TASK1_TM_RESPONSE'],
+        'tasks': ['task1'],
+        'years': [2020],
+        },
+    'end_before_month': {
+        'name': 'end_before_month',
+        'schemas': ['AIDA_PHASE2_TASK1_TM_RESPONSE'],
+        'tasks': ['task1'],
+        'years': [2020],
+        },
+    'end_before_year': {
+        'name': 'end_before_year',
+        'schemas': ['AIDA_PHASE2_TASK1_TM_RESPONSE'],
+        'tasks': ['task1'],
+        'years': [2020],
+        },
+    'end_after_day': {
+        'name': 'end_after_day',
+        'schemas': ['AIDA_PHASE2_TASK1_TM_RESPONSE'],
+        'tasks': ['task1'],
+        'years': [2020],
+        },
+    'end_after_month': {
+        'name': 'end_after_month',
+        'schemas': ['AIDA_PHASE2_TASK1_TM_RESPONSE'],
+        'tasks': ['task1'],
+        'years': [2020],
+        },
+    'end_after_year': {
+        'name': 'end_after_year',
+        'schemas': ['AIDA_PHASE2_TASK1_TM_RESPONSE'],
+        'tasks': ['task1'],
+        'years': [2020],
         },
     'justification_confidence': {
         'name': 'justification_confidence',
-        'years': [2019],
+        'schemas': ['AIDA_PHASE2_TASK1_CM_RESPONSE'],
         'tasks': ['task1'],
-        'query_types': ['ClassQuery'],
-        'validate': 'validate_confidence'      
+        'validate': 'validate_confidence',
+        'years': [2020],
         },
     'kb_document_id': {
         'name': 'kb_document_id',
-        'years': [2019],
+        'schemas': ['AIDA_PHASE2_TASK1_AM_RESPONSE', 'AIDA_PHASE2_TASK1_CM_RESPONSE'],
         'tasks': ['task1'],
-        'query_types': ['ClassQuery'],
         'generate': 'generate_kb_document_id',
         'validate': 'validate_kb_document_id',
+        'years': [2020],
         },
-    'entity_type_in_response': {
-        'name': 'entity_type_in_response',
-        'years': [2019],
+    'mention_span': {
+        'name': 'mention_span',
+        'schemas': ['AIDA_PHASE2_TASK1_CM_RESPONSE'],
         'tasks': ['task1'],
-        'query_types': ['ClassQuery'],
-        'validate': 'validate_entity_type_in_response',
-        'normalize': 'normalize_entity_type',
-        'dependencies': ['query']
+        'validate': 'validate_value_provenance_triple',
+        'years': [2020],
         },
-    'query': {
-        'name': 'query',
-        'years': [2019],
+    'metatype': {
+        'name': 'metatype',
+        'years': [2020],
+        'schemas': ['AIDA_PHASE2_TASK1_AM_RESPONSE', 'AIDA_PHASE2_TASK1_CM_RESPONSE'],
         'tasks': ['task1'],
-        'query_types': ['ClassQuery'],
-        'generate': 'generate_query',
-        'dependencies': ['query_id']
+        'validate': 'validate_metatype',
         },
-    'entity_type_in_query': {
-        'name': 'entity_type_in_query',
-        'years': [2019],
+    'object_cluster_id': {
+        'name': 'object_cluster_id',
+        'schemas': ['AIDA_PHASE2_TASK1_AM_RESPONSE'],
         'tasks': ['task1'],
-        'query_types': ['ClassQuery'],
-        'validate': 'validate_entity_type_in_query',
-        'normalize': 'normalize_entity_type',
-        'dependencies': ['query']
+        'years': [2020],
         },
-    'query_id': {
-        'name': 'query_id',
-        'years': [2019],
+    'object_informative_justification': {
+        'name' : 'object_informative_justification',
+        'schemas': ['AIDA_PHASE2_TASK1_AM_RESPONSE'],
         'tasks': ['task1'],
-        'query_types': ['ClassQuery'],
-        'generate': 'generate_query_id',
-        'required': 1,
+        'validate': 'validate_value_provenance_triple',
+        'years': [2020],
+        },
+    'object_justification_confidence': {
+        'name' : 'object_justification_confidence',
+        'schemas': ['AIDA_PHASE2_TASK1_AM_RESPONSE'],
+        'tasks': ['task1'],
+        'validate': 'validate_confidence',
+        'years': [2020],
+        },
+    'predicate': {
+        'name': 'predicate',
+        'schemas': ['AIDA_PHASE2_TASK1_AM_RESPONSE'],
+        'tasks': ['task1'],
+        'validate': 'validate_predicate',
+        'years': [2020],
+        },
+    'predicate_justification': {
+        'name': 'predicate_justification',
+        'schemas': ['AIDA_PHASE2_TASK1_AM_RESPONSE'],
+        'tasks': ['task1'],
+        'validate': 'validate_value_provenance_triple',
+        'years': [2020],
+        },
+    'predicate_justification_confidence': {
+        'name': 'predicate_justification_confidence',
+        'schemas': ['AIDA_PHASE2_TASK1_AM_RESPONSE'],
+        'tasks': ['task1'],
+        'validate': 'validate_confidence',
+        'years': [2020],
+        },
+    'start_before_day': {
+        'name': 'start_before_day',
+        'schemas': ['AIDA_PHASE2_TASK1_TM_RESPONSE'],
+        'tasks': ['task1'],
+        'years': [2020],
+        },
+    'start_before_month': {
+        'name': 'start_before_month',
+        'schemas': ['AIDA_PHASE2_TASK1_TM_RESPONSE'],
+        'tasks': ['task1'],
+        'years': [2020],
+        },
+    'start_before_year': {
+        'name': 'start_before_year',
+        'schemas': ['AIDA_PHASE2_TASK1_TM_RESPONSE'],
+        'tasks': ['task1'],
+        'years': [2020],
+        },
+    'start_after_day': {
+        'name': 'start_after_day',
+        'schemas': ['AIDA_PHASE2_TASK1_TM_RESPONSE'],
+        'tasks': ['task1'],
+        'years': [2020],
+        },
+    'start_after_month': {
+        'name': 'start_after_month',
+        'schemas': ['AIDA_PHASE2_TASK1_TM_RESPONSE'],
+        'tasks': ['task1'],
+        'years': [2020],
+        },
+    'start_after_year': {
+        'name': 'start_after_year',
+        'schemas': ['AIDA_PHASE2_TASK1_TM_RESPONSE'],
+        'tasks': ['task1'],
+        'years': [2020],
+        },
+    'subject_cluster_id': {
+        'name': 'subject_cluster_id',
+        'schemas': ['AIDA_PHASE2_TASK1_AM_RESPONSE'],
+        'tasks': ['task1'],
+        'years': [2020],
         },
     'type_confidence': {
         'name': 'type_confidence',
-        'years': [2019],
+        'schemas': ['AIDA_PHASE2_TASK1_CM_RESPONSE'],
         'tasks': ['task1'],
-        'query_types': ['ClassQuery'],
-        'validate': 'validate_confidence'      
+        'validate': 'validate_confidence',
+        'years': [2020],
         },
-    'value_provenance_triple': {
-        'name': 'value_provenance_triple',
-        'years': [2019],
-        'tasks': ['task1'],
-        'query_types': ['ClassQuery'],
-        'validate': 'validate_value_provenance_triple'        
-        }
     }
 
 schemas = {
-    '2019_TA1_CL_SUBMISSION': {
-        'year': 2019,
+    'AIDA_PHASE2_TASK1_AM_RESPONSE': {
+        'name': 'AIDA_PHASE2_TASK1_AM_RESPONSE',
+        'year': 2020,
         'task': 'task1',
-        'query_type': 'ClassQuery',
-        'file_type': 'submission',
-        'header': ['?docid', '?query_type', '?cluster', '?type', '?infj_span', '?t_cv', '?cm_cv', '?j_cv'],
+        'header': ['?metatype', '?subject', '?predicate', '?object', '?object_inf_j_span', '?ej_span', '?edge_cj_cv', '?edge_cv', '?obj_inf_j_cv'],
         'columns': [
-            'document_id',
-            'entity_type_in_query',
+            'metatype',
+            'subject_cluster_id',
+            'predicate',
+            'object_cluster_id',
+            'object_informative_justification',
+            'predicate_justification',
+            'predicate_justification_confidence',
+            'argument_assertion_confidence',
+            'object_justification_confidence'
+            ]
+        },
+    'AIDA_PHASE2_TASK1_CM_RESPONSE': {
+        'name': 'AIDA_PHASE2_TASK1_CM_RESPONSE',
+        'year': 2020,
+        'task': 'task1',
+        'header': ['?cluster', '?metatype', '?type', '?mention_span', '?t_cv', '?cm_cv', '?j_cv'],
+        'columns': [
             'cluster_id',
-            'entity_type_in_response',
-            'value_provenance_triple',
+            'metatype',
+            'cluster_type',
+            'mention_span',
             'type_confidence',
             'cluster_membership_confidence',
             'justification_confidence'
             ]
+        },
+    'AIDA_PHASE2_TASK1_TM_RESPONSE': {
+        'name': 'AIDA_PHASE2_TASK1_TM_RESPONSE',
+        'year': 2020,
+        'task': 'task1',
+        'header': ['?cluster', '?sa_month', '?sa_day', '?sa_year', '?sb_month', '?sb_day', '?sb_year', '?ea_month', '?ea_day', '?ea_year', '?eb_month', '?eb_day', '?eb_year'],
+        'columns': [
+            'cluster_id',
+            'start_after_month',
+            'start_after_day',
+            'start_after_year',
+            'start_before_month',
+            'start_before_day',
+            'start_before_year',
+            'end_after_month',
+            'end_after_day',
+            'end_after_year',
+            'end_before_month',
+            'end_before_day',
+            'end_before_year'
+            ]
         }
     }
 
-def identify_file_schema(query_id):
-    schema_name = None
-    if 'AIDA_TA1_CL_2019' in query_id: schema_name = '2019_TA1_CL_SUBMISSION'
-    return schema_name
+def identify_file_schema(fh):
+    for schema in schemas.values():
+        found = 1
+        if len(schema['header']) == len(fh.get('header').get('columns')):
+            for i in range(len(schema['header'])):
+                if schema['header'][i] != fh.get('header').get('columns')[i]:
+                    found = 0
+                    break
+            if found: return schema
+    return None
 
 class ResponseSet(Container):
     """
     Set of responses for AIDA.
     """
 
-    def __init__(self, logger, queries, document_mappings, text_boundaries, image_boundaries, keyframe_boundaries, queries_to_score, path, runid):
+    def __init__(self, logger, document_mappings, document_boundaries, path, runid):
         super().__init__(logger)
-        self.queries = queries
         self.document_mappings = document_mappings
-        self.text_boundaries = text_boundaries
-        self.image_boundaries = image_boundaries
-        self.keyframe_boundaries = keyframe_boundaries
-        self.queries_to_score = queries_to_score
+        self.document_boundaries = document_boundaries
         self.validator = Validator(logger)
         self.generator = Generator(logger)
         self.normalizer = Normalizer(logger)
-        self.categorized_responses = Container(logger)
+        self.responses = Container(logger)
         self.runid = runid
         self.path = path
-        self.run_dir = '{}/{}/sparql-valid-output'.format(path, runid)
-        self.cas_dir = '{}/{}/valid-ca-output'.format(path, runid)
-        for subdir_name in os.listdir(self.run_dir):
-            subdir = '{}/{}'.format(self.run_dir, subdir_name)
-            if not os.path.isdir(subdir): continue
-            for filename in os.listdir(subdir):
-                sparql_output_filename = '{}/{}/{}'.format(self.run_dir, subdir_name, filename)
-                ca_output_filename = '{}/{}/{}'.format(self.cas_dir, subdir_name, filename)
-                query_id = filename.rstrip()
-                query_id = query_id[:-7]
-                # skip the query if the query is not in the queries file,
-                #   or if it is not to be used for scoring
-                if query_id not in queries or query_id not in queries_to_score:
-                    logger.record_event('SKIPPING_FILE', sparql_output_filename, self.get('code_location'))
-                    continue
-                schema_name = identify_file_schema(query_id)
-                if schema_name not in schemas:
-                    logger.record_event('UNKNOWN_RESPONSE_FILE_TYPE', sparql_output_filename, self.get('code_location'))
-                schema = schemas[schema_name]
-                self.load_sparql_output_file(schema, sparql_output_filename)
-                self.load_ca_output_file(ca_output_filename)
+        self.load_responses()
 
-    def load_sparql_output_file(self, schema, filename):
-        logger = self.logger
-        fh = FileHandler(logger, filename)
-        expected_columns = schema.get('header')
-        provided_columns = fh.get('header').get('columns')
-        if len(expected_columns) != len(provided_columns):
-            logger.record_event('UNEXPECTED_NUM_OF_COLUMNS', len(expected_columns),len(provided_columns),
-                                {'filename': filename, 'lineno': 1})
-        for i in range(len(expected_columns)):
-            if expected_columns[i] != provided_columns[i]:
-                logger.record_event('UNEXPECTED_COLUMN_HEADER', i+1, expected_columns[i], provided_columns[i],
-                                    {'filename': filename, 'lineno': 1})
+    def load_responses(self):
+        logger = self.get('logger')
+        for subdir in ['{}/{}'.format(self.get('path'), d) for d in os.listdir(self.get('path'))]:
+            for filename in os.listdir(subdir):
+                filename_including_path = '{}/{}'.format(subdir, filename)
+                fh = FileHandler(logger, filename_including_path)
+                schema = identify_file_schema(fh)
+                if schema is None:
+                    logger.record_event('UNKNOWN_RESPONSE_FILE_TYPE', filename_including_path, self.get('code_location'))
+                self.load_file(fh, schema)
+
+    def load_file(self, fh, schema):
+        logger = self.get('logger')
         for entry in fh:
-            filename_and_lineno = '{}:{}'.format(entry.get('filename'), entry.get('lineno'))
+            filename = entry.get('filename')
+            lineno = entry.get('lineno')
             entry.set('runid', self.get('runid'))
             entry.set('schema', schema)
-            for i in range(len(expected_columns)):
-                entry.set(schema.get('columns')[i], entry.get(expected_columns[i]))
+            for i in range(len(schema.get('columns'))):
+                entry.set(schema.get('columns')[i], entry.get(entry.get('header').get('columns')[i]))
             valid = True
             for attribute_name in attributes:
                 attribute = attributes[attribute_name]
-                if attribute is None:
-                    self.record_event('NO_SPECS', attribute_name, self.get_code_location())
                 # skip if the attribute is not required for the given schema
                 if not self.attribute_required(attribute, schema): continue
                 # generate value for the attribute, if needed
@@ -202,47 +331,16 @@ class ResponseSet(Container):
                     valid_attribute = self.get('validator').validate(self, validator_name, schema, entry, attribute)
                     if not valid_attribute: valid = False
             entry.set('valid', valid)
-            self.add(key=filename_and_lineno, value=entry)
-            # categorize responses in order to make it easy for attaching confidence aggregation values later
-            code = self.queries.get('TASK_AND_TYPE_CODE')
-            if code == 'TA1_CL':
-                query_id = entry.get('query_id')
-                kb_document_id = entry.get('kb_document_id')
-                cluster_id = entry.get('cluster_id')
-                key = '::'.join([query_id, kb_document_id, cluster_id])
-                self.get('categorized_responses').get(key, default=Container(self.logger)).add(key=key, value=entry)
-            elif code == 'TA1_GR':
-                uuid = get_md5_from_string(entry.get('line'))
-                if self.get('categorized_responses').exists(uuid):
-                    self.record_event('DUPLICATE_VALUE', entry.get('line'), entry.get('where'))
-                self.get('categorized_responses').add(key=uuid, value=entry)
-            
-    
-    def load_ca_output_file(self, filename):
-        code = self.queries.get('TASK_AND_TYPE_CODE')
-        method_name = 'load_ca_output_file_{}'.format(code)
-        method = self.get_method(method_name)
-        if method is None:
-            self.logger('UNDEFINED_METHOD', method_name, self.get_code_location())
-        method(filename)
-    
-    def load_ca_output_file_TA1_CL(self, filename):
-        kb_document_id = get_kb_document_id_from_filename(filename)
-        query_id = get_query_id_from_filename(filename)
-        for entry in FileHandler(self.logger, filename):
-            cluster_id = entry.get('?cluster')
-            rank = entry.get('?rank')
-            key = '::'.join([query_id, kb_document_id, cluster_id])
-            for response in self.get('categorized_responses').get(key).values():
-                response.set('cluster_rank', rank)
-    
+            if not self.exists(filename):
+                self.add(key=filename, value=Container(logger))
+            self.get(filename).add(key=str(lineno), value=entry)
+
     def attribute_required(self, attribute, schema):
         year = schema.get('year')
         task = schema.get('task')
-        query_type = schema.get('query_type')
         if year not in attribute.get('years'): return False
         if task not in attribute.get('tasks'): return False
-        if query_type not in attribute.get('query_types'): return False
+        if schema.get('name') not in attribute.get('schemas'): return False
         return True
 
     def generate_value(self, attribute, entry):
@@ -256,3 +354,20 @@ class ResponseSet(Container):
         generator_name = attribute.get('generate')
         if generator_name:
             self.get('generator').generate(self, generator_name, entry)
+
+    def write_valid_responses(self, output_dir):
+        os.mkdir(output_dir)
+        for input_filename in self:
+            output_filename = input_filename.replace(self.get('path'), output_dir)
+            dirname = os.path.dirname(output_filename)
+            if not os.path.exists(dirname):
+                os.mkdir(dirname)
+            output_fh = open(output_filename, 'w')
+            header_printed = False
+            for linenum in sorted(self.get(input_filename), key=int):
+                entry = self.get(input_filename).get(str(linenum))
+                if not header_printed:
+                    output_fh.write('{}\n'.format(entry.get('header').get('line')))
+                    header_printed = True
+                output_fh.write(entry.__str__())
+            output_fh.close()
