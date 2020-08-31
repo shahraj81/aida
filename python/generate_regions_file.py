4dfe45d9afcbf9f5141b383897bc65bdf09a30bd
Shahzad Rajput <shahzad.rajput@gmail.com>
2020-08-31 15:59:49 -0400

    throw a critical error when the propercased full type is not found

--- python/generate_regions_file.py
+++ python/generate_regions_file.py
@@ -14,6 +14,7 @@ from aida.logger import Logger
 from aida.container import Container
 from aida.text_boundaries import TextBoundaries
 from aida.video_boundaries import VideoBoundaries
+from aida.utility import multisort
 
 import argparse
 import os
@@ -45,11 +46,6 @@ def check_for_paths_non_existance(paths):
         if os.path.exists(path):
             print('Error: Path {} exists'.format(path))
             exit(ERROR_EXIT_CODE)
-
-def multisort(xs, specs):
-    for key, reverse in reversed(specs):
-        xs.sort(key=lambda x: x[key], reverse=reverse)
-    return xs
 
 def get_line(output_object, columns):
     document_element_or_keyframe_id = output_object.get('keyframe_id') if output_object.get('keyframe_id') else output_object.get('document_element_id')
@@ -85,6 +81,8 @@ def main(args):
         full_type = '{type}.{subtype}.{subsubtype}'.format(type=type, subtype=subtype, subsubtype=subsubtype)
         full_type_cleaned = full_type.replace('.unspecified', '')
         propercased_full_type = type_mappings.get(full_type_cleaned, None)
+        if propercased_full_type is None:
+            logger.record_event('DEFAULT_CRITICAL_ERROR', 'propercased_full_type is None for full_type: {}'.format(full_type))
         span_string = entry.get('span')
         keyframe_id = None
         keyframe_num = 0
