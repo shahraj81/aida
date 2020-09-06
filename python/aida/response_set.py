4aaf10d02d86c80e7bf6f444d2f2bfc1d46bd38b
Shahzad Rajput <shahzad.rajput@gmail.com>
2020-09-05 20:00:45 -0400

    record error in log if file is found without header (i.e. empty file)

--- python/aida/response_set.py
+++ python/aida/response_set.py
@@ -344,7 +344,9 @@ schemas = {
 def identify_file_schema(fh):
     for schema in schemas.values():
         found = 1
-        if len(schema['header']) == len(fh.get('header').get('columns')):
+        if fh.get('header') is None:
+            fh.record_event('EMPTY_FILE_WITHOUT_HEADER', fh.get('filename'), '\t'.join(schema['header']))
+        elif len(schema['header']) == len(fh.get('header').get('columns')):
             for i in range(len(schema['header'])):
                 if schema['header'][i] != fh.get('header').get('columns')[i]:
                     found = 0
